2d479a0c9a4fb54bc536a01bab70a0e9a8701bce
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2010-02-16 22:12:39 +0900

    GuardConditions#isに関するテストの追加

--- test/gcb/core/GuardConditionsTest.java
+++ test/gcb/core/GuardConditionsTest.java
@@ -1,14 +1,26 @@
+/**
+ *
+ */
 package gcb.core;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
+/**
+ * @author bleis-tift
+ *
+ */
 public class GuardConditionsTest {
 
 	@Test
 	public void testIs() {
 		assertThat(GuardConditions.is(0).match(0), is(true));
+		assertThat(GuardConditions.is(1).match(0), is(false));
+		// 両方nullならtrue
+		assertThat(GuardConditions.is(null).match(null), is(true));
+		// 一方のみnullならfalse
+		assertThat(GuardConditions.is(null).match(10), is(false));
+		assertThat(GuardConditions.is(10).match(null), is(false));
 	}
 }
